bbbbfb9f8e2783dade2f8d92a560ee42f1839e2d
semanticdreams <188935636+semanticdreams@users.noreply.github.com>
2026-03-15 23:00:02 +0100

    fix(ci): prefer cpp as mingw windres preprocessor

--- scripts/vcpkg-triplets/mingw-posix-toolchain.cmake
+++ scripts/vcpkg-triplets/mingw-posix-toolchain.cmake
@@ -5,12 +5,22 @@ if(CMAKE_HOST_WIN32)
     set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc.exe CACHE STRING "")
     set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++.exe CACHE STRING "")
     set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres.exe CACHE STRING "")
-    find_program(SPACE_RC_PREPROCESSOR_PATH NAMES ${CMAKE_C_COMPILER} REQUIRED)
+    find_program(SPACE_RC_PREPROCESSOR_PATH
+        NAMES x86_64-w64-mingw32-cpp.exe x86_64-w64-mingw32-cpp cpp.exe cpp
+    )
+    if(NOT SPACE_RC_PREPROCESSOR_PATH)
+        find_program(SPACE_RC_PREPROCESSOR_PATH NAMES ${CMAKE_C_COMPILER} REQUIRED)
+    endif()
 else()
     set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc-posix CACHE STRING "")
     set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++-posix CACHE STRING "")
     set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres CACHE STRING "")
-    set(SPACE_RC_PREPROCESSOR_PATH ${CMAKE_C_COMPILER})
+    find_program(SPACE_RC_PREPROCESSOR_PATH
+        NAMES x86_64-w64-mingw32-cpp x86_64-w64-mingw32-cpp-posix cpp
+    )
+    if(NOT SPACE_RC_PREPROCESSOR_PATH)
+        set(SPACE_RC_PREPROCESSOR_PATH ${CMAKE_C_COMPILER})
+    endif()
 endif()
 set(CMAKE_RC_FLAGS_INIT "--preprocessor=${SPACE_RC_PREPROCESSOR_PATH}" CACHE STRING "")
 
